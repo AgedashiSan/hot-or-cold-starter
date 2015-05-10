2b19d8ff21a8a1597239418e4b8465764f26f48e
AgedashiSan <chan.silas@gmail.com>
2015-05-09 22:42:06 -0700

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,48 @@
 
 $(document).ready(function(){
+
+	var keyWord = Math.floor(Math.random()*100 + 1);
+	var i = 0;
+	
+	$(".guessButton").click(function(){
+		i = i++;
+		var guessWord = document.getElementById("userGuess").value;
+		document.getElementById("count").innerHTML = i;
+		var delta = new Array();
+		delta[i] = Math.abs(guessWord - keyWord);
+		$("@guessList").append("<li>" + guessWord + "</li>");
+		if (delta[i]==0) {
+			document.getElementById("feedback").innerHTML = "Yeah you got it!!";
+		}
+		else if (i == 1) {
+			if (delta[i] > 50) {
+				document.getElementById("feedback").innerHTML = "Too Cold!";
+			}
+			else if (delta[i] > 30) {
+				document.getElementById("feedback").innerHTML = "Cold!";
+			}
+			else if (delta[i] > 20) {
+				document.getElementById("feedback").innerHTML = "Warm!";
+			}
+			else if (delta[i] > 10) {
+				document.getElementById("feedback").innerHTML = "Hot!";
+			}
+			else {
+				document.getElementById("feedback").innerHTML = "Very Hot!";
+			}
+		}
+		else {
+			if (delta[i] > delta[i-1]) {
+				document.getElementById("feedback").innerHTML = "It's colder!";
+			}
+			else if (elta[i] < delta[i-1]) {
+				document.getElementById("feedback").innerHTML = "It's hotter!";
+			}
+			else {
+				document.getElementById("feedback").innerHTML = "Hmm it's about the same...";
+			}
+		}
+	};
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
